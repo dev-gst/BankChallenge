a01d285fd74de95265fd2b9809075bbd98ffa25f
Gustavo Garcia <gst.garc@gmail.com>
2025-01-23 17:08:01 -0300

    feat: create basic DAO class that can be used by the services

---
+++ src/main/java/br/com/compass/repository/dao/BaseDAO.java
@@ -0,0 +1,36 @@
+package br.com.compass.repository.dao;
+
+import br.com.compass.repository.BasicCRUD;
+import jakarta.persistence.EntityManager;
+
+public class BaseDAO<T> implements BasicCRUD<T> {
+
+    private final EntityManager entityManager;
+    private final Class<T> entityClass;
+
+    public BaseDAO(EntityManager entityManager, Class<T> entityClass) {
+        this.entityManager = entityManager;
+        this.entityClass = entityClass;
+    }
+
+    @Override
+    public T findById(Long id) {
+        return entityManager.find(entityClass, id);
+    }
+
+    @Override
+    public void save(T entity) {
+        entityManager.persist(entity);
+    }
+
+    @Override
+    public void update(T entity) {
+        entityManager.merge(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        entityManager.remove(entity);
+    }
+
+}
