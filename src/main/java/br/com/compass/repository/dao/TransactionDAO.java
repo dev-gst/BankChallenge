0343d0c3df1443f9e13d54f24afe821e3fe602a2
Gustavo Garcia <gst.garc@gmail.com>
2025-01-28 13:12:39 -0300

    feat: update TransactionDAO with new method findByAccount

--- src/main/java/br/com/compass/repository/dao/TransactionDAO.java
+++ src/main/java/br/com/compass/repository/dao/TransactionDAO.java
@@ -1,7 +1,11 @@
 package br.com.compass.repository.dao;
 
+import br.com.compass.model.Account;
 import br.com.compass.model.Transaction;
 import jakarta.persistence.EntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionDAO extends BaseDAO<Transaction> {
 
@@ -10,6 +14,24 @@ public class TransactionDAO extends BaseDAO<Transaction> {
     public TransactionDAO(EntityManager entityManager) {
         super(entityManager, Transaction.class);
         this.entityManager = entityManager;
+    }
+
+    public List<Transaction> findByAccount(Account account) {
+        String query = """
+            SELECT t
+            FROM Transaction t
+            WHERE t.sourceAccount = :account
+             OR t.destinationAccount = :account
+        """;
+
+        try {
+            return entityManager.createQuery(query, Transaction.class)
+                    .setParameter("account", account)
+                    .getResultList();
+        } catch (Exception e) {
+            return new ArrayList<>();
+        }
+
     }
 
 }
