7f85233a7110db061643f035121abff006f80f54
Gustavo Garcia <gst.garc@gmail.com>
2025-01-28 00:38:06 -0300

    feat: add basic feature of creating and saving client with user input

---
+++ src/main/java/br/com/compass/service/ClientService.java
@@ -0,0 +1,55 @@
+package br.com.compass.service;
+
+import br.com.compass.model.Client;
+import br.com.compass.repository.dao.BaseDAO;
+import br.com.compass.util.validation.ClientInputCollector;
+
+import java.time.LocalDate;
+
+public class ClientService {
+
+    private final BaseDAO<Client> clientDAO;
+    private final ClientInputCollector collector;
+
+    public ClientService( ClientInputCollector collector, BaseDAO<Client> clientDAO) {
+        this.clientDAO = clientDAO;
+        this.collector = collector;
+    }
+
+    public void createClient() {
+        Client client = getUserInfo();
+        try {
+            clientDAO.startTransaction();
+            clientDAO.save(client);
+            clientDAO.commitTransaction();
+        } catch (Exception e) {
+            clientDAO.rollbackTransaction();
+            System.out.println("An error occurred while creating the client.");
+        }
+    }
+
+    private Client getUserInfo() {
+        Client client;
+
+        String firstName = collector.collectName("Enter your first name: ");
+        String lastName = collector.collectName("Enter your last name: ");
+        String phone = collector.collectPhone("Enter your phone: ");
+        String email = collector.collectEmail("Enter your email: ");
+        String password = collector.collectPassword("Enter your password: ");
+        String cpf = collector.collectCPF("Enter your cpf: ");
+        LocalDate birthDate = collector.collectBirthDate("Enter your birth date: ");
+
+        client = new Client.Builder()
+                .withFirstName(firstName)
+                .withLastName(lastName)
+                .withPhone(phone)
+                .withEmail(email)
+                .withPassword(password)
+                .withBirthDate(birthDate)
+                .withCpf(cpf)
+                .build();
+
+        return client;
+    }
+
+}
