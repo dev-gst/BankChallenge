b92a5239bd8f2069cad468024a183222daa4b081
Gustavo Garcia <gst.garc@gmail.com>
2025-01-28 02:15:10 -0300

    fix: AccountType validation on AccountInputCollector

--- src/main/java/br/com/compass/util/validation/AccountInputCollector.java
+++ src/main/java/br/com/compass/util/validation/AccountInputCollector.java
@@ -1,14 +1,14 @@
 package br.com.compass.util.validation;
 
-import br.com.compass.model.Account;
+import br.com.compass.model.enumeration.AccountType;
 
 public class AccountInputCollector {
 
     private static final Validator accountTypeValidator = input -> {
         try {
-            Account.AccountType.valueOf(input.toUpperCase());
+            AccountType.fromCode(Integer.parseInt(input));
             return true;
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException ignored) {
             return false;
         }
     };
@@ -19,9 +19,9 @@ public class AccountInputCollector {
         this.collector = collector;
     }
 
-    public Account.AccountType collectAccountType(String message) {
+    public AccountType collectAccountType(String message) {
         String rawAccountType = collector.collectInput(message, accountTypeValidator);
-        return Account.AccountType.valueOf(rawAccountType.toUpperCase());
+        return AccountType.fromDescription(rawAccountType.toUpperCase());
     }
 
 }
